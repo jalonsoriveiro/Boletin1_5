d3af637c1a0fb5466a9b1f5a0c9e078e98f6d062
Jose Alonso <jalonsoriveiro@danielcastelao.org>
2019-10-08 09:05:27 +0200

    Inicio

---
+++ src/bol1_5/Bol1_5.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bol1_5;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author jalonsoriveiro
+ */
+public class Bol1_5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //    5- Escribe un programa que lea o valor dunha distancia en millas mariñas e a pase a metros ( 1 milla mariña = 1852 m ).
+        //Codifica o programa correspondente para executar o programa
+
+        float valorMillaMetro = 1852;
+        
+        System.out.print("Millas =");
+        Scanner milla= new Scanner(System.in);
+        float millas = milla.nextFloat();  
+        System.out.println("");         
+        System.out.print(" Valor millas ="+valorMillaMetro*millas);
+    }
+    
+}
